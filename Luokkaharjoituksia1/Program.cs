8755053cbc2ded6f11cb94f94a91763283aedf2c
HasanbasriUysal <48217@edu.raseko.fi>
2024-02-07 09:49:16 +0200

    added

--- Luokkaharjoituksia1/Program.cs
+++ Luokkaharjoituksia1/Program.cs
@@ -6,7 +6,6 @@ using System.Threading.Tasks;
 
 namespace Luokkaharjoituksia1
 {
-
     class Hooman
     {
         // Define properties of Hooman ie. fields
@@ -14,37 +13,53 @@ namespace Luokkaharjoituksia1
         int age = 30;
         string gender = "Emäntä";
 
-        Hooman()
+        // Default constructor w/o arguments
+        // No need to define, will be created automatically
+        public Hooman()
         {
 
         }
 
         // Constructor with one argument
-        Hooman(string name)
+        public Hooman(string name)
         {
             this.name = name;
         }
 
         // Constructor with 2 arguments
-        Hooman(string name, int age)
+        public Hooman(string name, int age)
         {
             this.name = name;
             this.age = age;
         }
 
         // Constructor with 3 arguments
-        Hooman(string name, int age, string gender)
+        public Hooman(string name, int age, string gender)
         {
             this.name = name;
             this.age = age;
             this.gender = gender;
 
         }
+    
+        // A method to say momethin
+        public void SayOpinion()
+        {
+            Console.WriteLine("Voi, lemmikit ne on elaman suola");
+        }
     }
     internal class Program
     {
         static void Main(string[] args)
         {
+            //Create (instantiate) a hooman object from Hooman class
+            Hooman owner = new Hooman("Ossi Omistaja", 35, "isäntä");
+
+            //Call the SayOpinion method
+            owner.SayOpinion();
+
+            //Keep the window opent until enter pressed
+            Console.ReadLine();
         }
     }
 }
